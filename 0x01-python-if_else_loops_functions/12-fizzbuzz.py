d38543c1968f9bc780ef40c40c1ade04d0557a8a
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-11-28 15:52:25 -0800

    12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fuzzbuzz():
+    for i in range(1, 101):
+        if i % 3 != 0 and i % 5 != 0:
+            print("{} ".format(i), end="")
+        else:
+            if i % 3 == 0:
+                print("Fizz", end="")
+            if i % 5 == 0:
+                print("Bizz", end="")
+            print(" ", end="")
+fuzzbuzz()
