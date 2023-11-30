6513a138bb8de726ee47bc50f4806e42a838fac8
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-11-30 13:05:18 -0800

    0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import add_0 as add_f
+from add_0 import add
 a = 1
 b = 2
-print("<{}> + <{}> = <{}>".format(a, b, add_f.add(a, b)))
+print("<{}> + <{}> = <{}>".format(a, b, add(a, b)))
