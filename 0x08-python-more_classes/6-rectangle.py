b589d73d0bb696c93fce783a5aa3f76cfab28410
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-01-06 14:54:37 -0800

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
         class that define a rectangle
     '''
 
-    number_of_instances=0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         self.width = width
