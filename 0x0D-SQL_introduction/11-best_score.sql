c7c95a91f8c7d973a2f1d1f8874b0be80f88ba3f
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-02-14 15:48:21 -0800

    task 12

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- list records with score >= 10 feom second table
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- update bob score to 10
+UPDATE second_table set score = 10 where name = 'Bob';
