f65e96ecff3e38b3608be8de411621d2901dde85
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-02-15 13:46:31 -0800

    task 11

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- lists all cities (cities.id, cities.name, states.names)
+SELECT cities.id, cities.name, cities.name 
+FROM cities INNER JOIN states ON states.id = cities.state_id
+ORDER BY cities.id;
