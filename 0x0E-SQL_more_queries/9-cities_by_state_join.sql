8de30852d277bf1e2e612f03b775a6255f1fe84e
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-02-15 13:50:33 -0800

    task 11

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities (cities.id, cities.name, states.names)
-SELECT cities.id, cities.name, cities.name 
+SELECT cities.id, cities.name, states.name 
 FROM cities LEFT JOIN states ON states.id = cities.state_id
 ORDER BY cities.id;
