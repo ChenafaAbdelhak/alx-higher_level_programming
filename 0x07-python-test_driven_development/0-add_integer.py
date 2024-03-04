74c1c6f483f937236835f96e1abfd7d6e751d7a9
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-03 23:35:42 -0800

    fix pycodstyle t0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,14 +2,14 @@
 '''module to add integers'''
 
 
-def add_integer(a, b=98): 
+def add_integer(a, b=98):
     """
     add two integer a + b
     """
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-   
+
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
