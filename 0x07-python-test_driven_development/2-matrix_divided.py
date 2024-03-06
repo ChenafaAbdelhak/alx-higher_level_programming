2b399b9751210cb429ae40b534237c724d549020
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-06 14:01:53 -0800

    task 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module that defines matrix_divided function"""
+
+
+def matrix_divided(matrix, div):
+    """
+    function that divide all elements of a matrix
+
+    Args:
+        matrix (list): a list of lists of ints or integers
+        div (int/float): the divisor
+    """
+    
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(element, int) or (isinstance(element, float)))
+                for row in matrix for element in row)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    if (not all(len(row) == len(matrix[0]) for row in matrix)):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
