6ece74abd53301bcb378cd53d855e5709590b638
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-07 15:42:33 -0800

    tassk 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' module to check if an instance is of a class type'''
+
+
+def is_same_class(obj, a_class):
+    '''
+    return true if obj is an instance of a_class
+    otherwise false
+    '''
+
+    if type(obj) == a_class:
+        return True
+    else:
+        false
