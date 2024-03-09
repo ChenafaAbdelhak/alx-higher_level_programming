682c9885bbf335f5db597529f537dd93ed0dc4f4
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-09 03:03:56 -0800

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class"""
+    pass
+
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate the value"""
+        if type(value) != int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
