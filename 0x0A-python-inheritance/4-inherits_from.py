143783e58d2d87b912a1a1b6e525aecd81b93f2c
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-09 02:45:36 -0800

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,7 +14,6 @@ def inherits_from(obj, a_class):
     Return:
         True or False
     """
-
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
