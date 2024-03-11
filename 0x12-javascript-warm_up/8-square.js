4f90491341f77fd3523a3f9e90c19beb9ea3d9ca
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-11 15:55:25 -0700

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+let asInt = parseInt(args[0], 10);
+let str = '';
+
+if (isNaN(asInt)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < asInt; i++) {
+    for (let i = 0; i < asInt; i++) {
+      str += 'X'
+    }
+    if (i != asInt - 1) {
+      str += '\n';
+    }
+  }
+    console.log(str);
+}
