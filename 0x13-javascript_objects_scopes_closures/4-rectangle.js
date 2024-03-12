2a30b450e3e39afc87e9d001d764add490fcbbc7
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-12 07:08:13 -0700

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let str = '';
+
+    for (let i = 0; i < this.height; i++) {
+      for (let i = 0; i < this.width; i++) {
+        str += 'X';
+      }
+
+      if (i !== this.height - 1) {
+        str += '\n';
+      }
+    }
+    if (str !== '') {
+      console.log(str);
+    }
+  }
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+}
+
+module.exports = Rectangle;
