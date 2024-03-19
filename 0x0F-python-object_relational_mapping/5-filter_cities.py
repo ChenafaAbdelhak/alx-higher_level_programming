c6d8e5514c209338f42498b3ef5e76d4d93523d2
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-18 17:36:58 -0700

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,12 +9,14 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+    cur.execute("""SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') FROM
                 cities INNER JOIN states ON states.id=cities.state_id AND
                 states.name=%s""",
                 (sys.argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
+    row = cur.fetchone()[0]
+    if row:
         print(row)
+    else:
+        print("")
     cur.close()
     db.close()
