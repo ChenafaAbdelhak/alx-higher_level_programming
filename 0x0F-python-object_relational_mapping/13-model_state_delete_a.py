1c9ea724fe8840dcfcdb7f14b0fa3dcaabce552c
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-22 11:29:27 -0700

    task13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""adds the State object “Louisiana” to the database hbtn_0e_6_usa"""
+"""
+deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa
+"""
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
@@ -16,7 +19,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
- 
+
     states = session.query(State).filter(State.name.like("%a%")).all()
 
     for state in states:
