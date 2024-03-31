eb5c63118a8ea7edfe86b4cc562acb7539d2e1bb
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-31 06:41:01 -0700

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        q = argv[1]
+    except IndexError:
+        q = ""
+
+    r = requests.post(url, data={"q": q})
+
+    print(r.text)
