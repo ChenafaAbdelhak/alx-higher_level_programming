f0ca92a370da6ab94fed0d461fca6c317e105218
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-31 05:33:48 -0700

    task 4

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# script that takes in a URL, sends a request to the URL and displays
-# the body of the response (decoded in utf-8).
+"""
+script that takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
 import urllib.error
---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+r = requests.get('https://alx-intranet.hbtn.io/status')
+
+print("Body responses:")
+print("\t- type: {}".format(type(r.type)))
+print("\t- content: {}".format(r.text))
+
