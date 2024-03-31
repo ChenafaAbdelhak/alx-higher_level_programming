758697c3a3f0cae2396a196b6b766e6514707e0e
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-03-31 05:40:01 -0700

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,9 +4,8 @@ script that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 
-r = requests.get('https://alx-intranet.hbtn.io/status')
+r = requests.get("https://alx-intranet.hbtn.io/status")
 
 print("Body responses:")
-print("\t- type: {}".format(type(r.text())))
+print("\t- type: {}".format(type(r.text)))
 print("\t- content: {}".format(r.text))
-
